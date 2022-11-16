1bf4646947a38d4fdba41a21072a964bcacf599e
sanwarhossain5 <sonymix5000@gmail.com>
2022-11-16 18:28:52 +0600

    more section added

--- src/Pages/Doctor/Doctor.js
+++ src/Pages/Doctor/Doctor.js
@@ -7,8 +7,8 @@ const Doctor = () => {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Doctor Her</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="mb-5 text-5xl font-bold">Doctor Hero</h1>
+      <p className="mb-5">A doctor is tasked with interacting with patients, diagnosing medical problems and successfully treating illness or injury. There are many specific areas in the field of medicine that students can study.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
